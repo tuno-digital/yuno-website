0fb2dc196f0fd1f02a55c3638fc809f7cad0dcca
tuno-digital <u1680341703@gmail.com>
2025-11-22 04:45:38 +0000

    Add request logger middleware for logging requests

--- server/utils/request-logger.js
+++ server/utils/request-logger.js
@@ -1,1 +1,10 @@
 
+// ===================================================
+// LOG DE REQUISIÇÕES — YUNO IA 10.3
+// ===================================================
+const logger = require("./logger");
+
+module.exports = function requestLogger(req, res, next) {
+    logger.info(`${req.method} ${req.url} — IP: ${req.ip}`);
+    next();
+};
